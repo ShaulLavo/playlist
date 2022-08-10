a7033f06800cb52b09ef82020e001022d76230e5
Shaul Lavochkin <shaul919191@gmail.com>
2022-08-10 13:23:58 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+
+  <RouterView />
+</template>
+
+
+
+<script>
+
+
+export default {
+
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+
+export const store = createStore({
+	strict: true,
+	state: {},
+	getters: {},
+	mutations: {},
+	actions: {},
+	modules: {}
+})
+
+export default store
